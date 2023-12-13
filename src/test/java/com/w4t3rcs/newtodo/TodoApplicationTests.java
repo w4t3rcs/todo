0d6a5f0589e9c31843c3ade0f0d46d09e51deb60
w4t3rcs <uimanovmaks@gmail.com>
2023-12-13 10:48:09 +0100

    Added Hello page and started coding SecurityConfig

---
+++ src/test/java/com/w4t3rcs/newtodo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.w4t3rcs.newtodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
