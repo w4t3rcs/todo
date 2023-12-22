3ed976b2ae360977edf563ab4adbeeebc2240cf0
w4t3rcs <uimanovmaks@gmail.com>
2023-12-22 23:40:28 +0100

    Added Task.finished field!

--- src/main/java/com/w4t3rcs/newtodo/model/entity/Task.java
+++ src/main/java/com/w4t3rcs/newtodo/model/entity/Task.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 import lombok.*;
+import org.hibernate.type.NumericBooleanConverter;
 import org.springframework.data.domain.Persistable;
 
 @ToString
@@ -23,6 +24,9 @@ public class Task implements Persistable<Long> {
     @NotEmpty(message = "Invalid description")
     @Size(max = 512, message = "Invalid description")
     private String description;
+    @Column(name = "is_finished")
+    @Convert(converter = NumericBooleanConverter.class)
+    private boolean finished;
 
     @Override
     public boolean isNew() {
