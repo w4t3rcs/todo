a20e00bfa4453461b52827e06e0ead0d2056b803
w4t3rcs <uimanovmaks@gmail.com>
2024-01-13 12:49:37 +0100

    Replaced images/logo.png with favicon.ico

--- src/test/java/com/w4t3rcs/newtodo/TodoApplicationTests.java
+++ src/test/java/com/w4t3rcs/newtodo/TodoApplicationTests.java
@@ -1,7 +1,5 @@
 package com.w4t3rcs.newtodo;
 
-import com.w4t3rcs.newtodo.model.common.executor.sender.MessageSender;
-import com.w4t3rcs.newtodo.model.service.sender.message.NotificationSender;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,7 +15,6 @@ class TodoApplicationTests {
 
     @Test
     void contextLoads() {
-        MessageSender sender = applicationContext.getBean(NotificationSender.class);
-        sender.send();
+
     }
 }
